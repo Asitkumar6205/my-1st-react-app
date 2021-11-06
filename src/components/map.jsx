a2643c9f8f51e83b9eb06627b106d90a6460a501
Asit Kumar <asit1234kumar3219@gmail.com>
2021-11-06 12:06:19 +0530

    first commit

---
+++ src/components/map.jsx
@@ -0,0 +1,8 @@
+import mapSvg from "../indiaHigh.svg"
+
+const Map=()=>{
+    return (
+        <img src={mapSvg} alt="map.svg" height="800px" width="100%"/>
+    )
+}
+export default Map;
---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import reportWebVitals from './reportWebVitals';
+import Data from './Data';
+import 'tachyons';
+
+ReactDOM.render(
+  <Data/>
+  ,document.getElementById('root')
+);
+
+reportWebVitals();
