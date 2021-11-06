76fc78bf2390d1b52b317f327b80057dc019e9b6
Asit Kumar <asit1234kumar3219@gmail.com>
2021-11-06 22:24:05 +0530

    Second Commit

--- src/IndiaMap.js
+++ src/IndiaMap.js
@@ -1,24 +1,35 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import India from "@svg-maps/india";
 import { SVGMap } from "react-svg-map";
 import "react-svg-map/lib/index.css";
+import axios from "axios";
 
 function IndiaMap() {
-  const [stateCode, setStateCode] = useState("");
-  const [stateName, setStateName] = useState("");
+  const [stateData, setStateData] = useState(null);
+  const [data, setData] = useState(null);
 
   function onLocationClick(event) {
-    setStateCode(event.target.id);
-    setStateName(event.target.getAttribute("name"));
+    let stateData=[]
+    if(data){
+      stateData=data.filter(state=>state.loc.toLowerCase()===event.target.getAttribute("name").toLowerCase())
+      setStateData(stateData[0]);
+    }
     // console.log(event.target);
     console.log("Id", event.target.id);
     console.log("Name", event.target.getAttribute("name"));
   }
 
+  useEffect(() => {
+    axios.get("https://api.rootnet.in/covid19-in/stats/latest").then((response)=>{
+      console.log(response.data.data.regional);
+      setData(response.data.data.regional);
+    })
+    return () => {}
+  }, [])
+
   return (
     <>
-      <p>{stateName}</p>
-      <p>{stateCode}</p>
+      <p>{JSON.stringify(stateData)}</p>
       <SVGMap map={India} onLocationClick={onLocationClick} />
     </>
   );
