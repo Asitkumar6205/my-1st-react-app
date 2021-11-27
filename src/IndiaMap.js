a0d8121d3b664a3424211b4ad385871c7cd455c3
Ashish Sharma <30octaksharma@gmail.com>
2021-11-27 23:52:12 +0530

    ui_fix

--- src/IndiaMap.js
+++ src/IndiaMap.js
@@ -3,10 +3,11 @@ import India from "@svg-maps/india";
 import { SVGMap } from "react-svg-map";
 import "react-svg-map/lib/index.css";
 import axios from "axios";
+import "./styles.css"
 
 function IndiaMap() {
   const [stateData, setStateData] = useState(0);
-  const [data, setData] = useState();
+  const [data, setData] = useState(0);
 //   confirmedCasesForeign: 0
 // confirmedCasesIndian: 7677
 // deaths: 129
@@ -16,33 +17,40 @@ function IndiaMap() {
   
 
   function onLocationClick(event) {
-    let stateData=[]
+    let stateData={}
     if(data){
       stateData = data.filter(state=>state.loc.toLowerCase() === event.target.getAttribute("name").toLowerCase())
       setStateData(stateData[0]);
     }
   }
 
-  useEffect(() => {
-    axios.get("https://api.rootnet.in/covid19-in/stats/latest").then((response)=>{
-      setData(response.data.data.regional);
+    useEffect(() => {
+      axios.get("https://api.rootnet.in/covid19-in/stats/latest").then((response)=>{
+        setData(response.data.data.regional);
+      })
+      return () => {}
     })
-    return () => {}
-  })
-
+    // console.log(stateData)
   return (
     <div>
+            <div className="card">
             {Object.keys(stateData).map((key)=>{
               return(
-                <>
-               
-                  {key + "=" + stateData[key]}
-                  <br/>
+                
+                  <li  className="list" key={key}>
+                  <div className="keys" >{key}</div>
+                  <div className="valueKey">{stateData[key]}</div>
+                  </li>
               
-                </>
+                
               )
             })}
-           <SVGMap map={India} onLocationClick={onLocationClick} />
+            </div>
+           <SVGMap 
+              map={India}
+              onLocationClick={onLocationClick}
+              
+              />
     </div>
   );
 }
